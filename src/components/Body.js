cf53e993eef49ec63d80cb52cd730c50de7b7d0b
Ritesh <Riteshk229@gmail.com>
2023-06-21 19:39:16 +0530

    basic design completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,26 @@
 import React from "react";
+import '../assets/CSS/body.css'
+
 
 class Body extends React.Component{
     render(){
         return(
-            <div>
-                This is body
+            <div id="outer-circle">
+                <button id='menu'>
+                    Menu
+                </button>
+                <button id='forward'>
+                    <i className="fa-solid fa-forward-fast"></i>
+                </button>
+                <button id='back'>
+                <i class="fa-solid fa-forward-fast fa-rotate-180"></i>
+                </button>
+                <button id='play-pause'>
+                    <i className="fa-solid fa-play"></i>
+                    <i className="fa-solid fa-pause"></i>
+                </button>
+                <div id = "inner-circle">
+                </div>
             </div>
         )
     }
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,13 +1,20 @@
 import React from "react";
-
+import '../assets/CSS/screen.css'
 class Screen extends React.Component{
     render(){
         return(
-            <div>
-                This is Screen
-            </div>
-        )
+            <>
+                <div id="screen">
+                    <div id="left">
+                        This is left Screen
+                    </div>
+                    <div id ="right">
+                        This is Right screen
+                    </div>
+                </div>
+            </>     
+            )
+        }
     }
-}
-
-export default Screen;
+    
+    export default Screen;
